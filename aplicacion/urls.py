98232112408398182f04092e2bde23c6048d3803
Iabin Arteaga <iabin@ciencias.unam.mx>
2018-09-23 18:46:38 -0500

    personalizada página no encontrada

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     #url(r'^course/(?P[0-9]+)/$', views.CourseList.as_view()),
     #url(r'^take/(?P[0-9]+)/$', views.TakeList.as_view()),
     url(r'^tomados/$', views.TakeList.as_view()),
-    url(r'^error/$', errorViews.noEncontrado),
+    url(r'^', errorViews.noEncontrado),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
